745e7b876373f1530d6822e9326a247469b818c9
tairqlearnstocode <tariq@greatlakespmg.com>
2025-03-17 15:06:38 -0400

    Fix Vercel build: Remove terser minifier requirement

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
     outDir: 'dist',
     target: 'es2015',
     sourcemap: false,
-    minify: 'terser',
     cssMinify: true,
     rollupOptions: {
       external: [
